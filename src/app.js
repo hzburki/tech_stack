a5e53aaceac8072de6bc1f821d136def2671ef37
Haseeb Burki <hzburki.hzb@gmail.com>
2017-07-23 15:19:00 +0500

    Recieved Libraries from Store via REdux and Rendered ListView

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ import { createStore } from 'redux';
 
 import reducers from './reducers';
 import { Header } from './components/common/';
+import LibraryList from './components/LibraryList';
 
 const App = () => (
   <Provider store={createStore(reducers)}>
-    <View>
+    <View style={{ flex: 1 }}>
       <Header headerText={'Technology Stack'} />
+      <LibraryList />
     </View>
   </Provider>
 );
